038523244b12264c89d125cb1b9db12a51011e54
Md. Zabaidul Islam <zislam5622@gmail.com>
2026-01-29 22:29:12 +0600

    add global error handler

---
+++ src/middleware/globalErrorHandler.ts
@@ -0,0 +1,52 @@
+import { NextFunction, Request, Response } from "express";
+import { Prisma } from "../../prisma/generated/prisma/client";
+
+function errorHandler(
+  err: any,
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) {
+  let statusCode = 500;
+  let errorMessage = "Internal Server Error";
+  let errorDetails = err;
+
+  // PrismaClientValidationError
+  if (err instanceof Prisma.PrismaClientValidationError) {
+    statusCode = 400;
+    errorMessage = "You provide incorrect field type or missing fields!";
+  }
+  // PrismaClientKnownRequestError
+  else if (err instanceof Prisma.PrismaClientKnownRequestError) {
+    if (err.code === "P2025") {
+      statusCode = 400;
+      errorMessage =
+        "An operation failed because it depends on one or more records that were required but not found.";
+    } else if (err.code === "P2002") {
+      statusCode = 400;
+      errorMessage = "Duplicate key error";
+    } else if (err.code === "P2003") {
+      statusCode = 400;
+      errorMessage = "Foreign key constraint failed";
+    }
+  } else if (err instanceof Prisma.PrismaClientUnknownRequestError) {
+    statusCode = 500;
+    errorMessage = "Error occurred during query execution";
+  } else if (err instanceof Prisma.PrismaClientInitializationError) {
+    if (err.errorCode === "P1000") {
+      statusCode = 401;
+      errorMessage = "Authentication failed. Please check your creditials!";
+    } else if (err.errorCode === "P1001") {
+      statusCode = 400;
+      errorMessage = "Can't reach database server";
+    }
+  }
+
+  res.status(statusCode);
+  res.json({
+    message: errorMessage,
+    error: errorDetails,
+  });
+}
+
+export default errorHandler;
